458fa8a5f81775470cd6415be13e2714c08b9b62
Jinho <yoonjinho0318@gmail.com>
2019-09-25 18:25:16 +0530

    Update format.

--- polynomial_features.m
+++ polynomial_features.m
@@ -2,12 +2,12 @@ function [X_added_features] = polynomial_features(X, degrees)
 
 n = size(X, 2);
 
-# Independent polynomial variables
+% Independent polynomial variables
 for d = 2:degrees
     X = [X, X .^ d];
 end
 
-# Intermingling of variables
+% Intermingling of variables
 for i = 1:degrees
     for j = i+1:degrees
         X = [X, X(:,i) .* X(:,j)];
--- splitdata.m
+++ splitdata.m
@@ -3,7 +3,6 @@ function [X_training, X_test, y_training, y_test] = splitdata(X, y)
 m = size(X, 1);
 n = size(X, 2);
 
-X = X(randperm(m), :);
 X_training = X(1:floor(0.8 * m), :);
 y_training = y(1:floor(0.8 * m));
 X_test = X(floor(0.8 * m):m, :);
