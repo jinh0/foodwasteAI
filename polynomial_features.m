1d842ba21f98a954ec78f2d2f9aa3b7f12af3f35
Jinho <yoonjinho0318@gmail.com>
2019-09-27 10:05:13 +0530

    Fix and simplify intermingling of variables.

--- polynomial_features.m
+++ polynomial_features.m
@@ -1,20 +1,24 @@
 % Creates more features from the already existing variables
 function [X_added_features] = polynomial_features(X, degrees)
 
-n = size(X, 2);
+initial_n = size(X, 2);
+
+X_add = [];
 
 % Create independent, separated polynomial variables
-for d = 2:degrees
-    X = [X, X .^ d];
-end
-
-% Intermingling of variables
-for i = 1:degrees
-    for j = i + 1:degrees
-        X = [X, X(:, i) .* X(:, j)];
+% 3 loops for the 3 initial columns
+for i = 0 : degrees % power for rice
+    for j = 0 : degrees % power for Dal
+        for k = 0 : degrees % power for students
+            X_add = [X_add, (X(:, 1) .^ i) .* (X(:, 2) .^ j) .* (X(:, 3) .^ k)];
+        end
     end
 end
 
-X_added_features = X;
+% Remove the column where all powers of variables are 0, which makes 1
+X_add = X_add(:, 2 : size(X_add, 2));
+
+% Merge initial X and X_add
+X_added_features = [X, X_add];
 
 end
